8d3ce4ead3a843e54ad067ffbb9bfde13b80b3a5
JFAldridge <jfaldridge1985@gmail.com>
2020-01-30 07:42:40 -0500

    Added styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :logged_in_user,    only: [:new, :create]
   def index
-    @posts = Post.all
+    @posts = Post.all.order("updated_at DESC")
   end
 
   def new
